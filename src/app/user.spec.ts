e3790b92cc7e960b7b6825cd95e728f30c80cab0
RishiSetpal <rishisetpal123@gmail.com>
2026-01-20 00:28:11 +0530

    Formatting and Adding Comments

--- src/app/user.spec.ts
+++ src/app/user.spec.ts
@@ -2,6 +2,6 @@ import { User } from './user';
 
 describe('User', () => {
   it('should create an instance', () => {
-    expect(new User()).toBeTruthy();
+    expect(new User('John', 'john@example.com', 1234567890, { street: '', city: '', zipCode: '' }, 'JavaScript', 'Morning', true)).toBeTruthy();
   });
 });
